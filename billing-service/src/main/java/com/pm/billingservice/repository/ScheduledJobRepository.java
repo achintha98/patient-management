8da746ad5a607959387fb58c0030df3fc79baa26
achintha98 <kalanaachintha@gmail.com>
2025-10-08 12:04:21 +0530

    Replaced entity manager with Scheduled job repository

--- billing-service/src/main/java/com/pm/billingservice/repository/ScheduledJobRepository.java
+++ billing-service/src/main/java/com/pm/billingservice/repository/ScheduledJobRepository.java
@@ -1,44 +1,15 @@
 package com.pm.billingservice.repository;
 
 import com.pm.billingservice.model.ScheduledJobRun;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * @author Achintha Kalunayaka
- * @since 10/7/2025
+ * @since 10/8/2025
  */
 
 @Repository
-public class ScheduledJobRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public boolean hasRunThisMonth(String jobName, LocalDateTime now) {
-        ScheduledJobRun jobRun = em.find(ScheduledJobRun.class, jobName);
-        if (jobRun == null) return false;
-
-        LocalDateTime lastRunDate = jobRun.getLastRun();
-        LocalDateTime firstDayOfMonth = now.withDayOfMonth(1);
-        return !lastRunDate.isBefore(firstDayOfMonth);
-    }
-
-    @Transactional
-    public void updateLastRun(String jobName, LocalDateTime now) {
-        ScheduledJobRun jobRun = em.find(ScheduledJobRun.class, jobName);
-        if (jobRun == null) {
-            em.persist(new ScheduledJobRun(jobName, now));
-        } else {
-            jobRun.setLastRun(now);
-            em.merge(jobRun);
-        }
-    }
+public interface ScheduledJobRepository extends JpaRepository<ScheduledJobRun, String> {
 
 }
