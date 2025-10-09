10812b3028c9839e69b595cf68afc592d2be2c66
achintha98 <kalanaachintha@gmail.com>
2025-10-09 23:48:10 +0530

    Added Invoice status enum

---
+++ billing-service/src/main/java/com/pm/billingservice/model/enums/InvoiceStatus.java
@@ -0,0 +1,14 @@
+package com.pm.billingservice.model.enums;
+
+/**
+ * @author Achintha Kalunayaka
+ * @since 10/9/2025
+ */
+public enum InvoiceStatus {
+
+    GENERATED,
+    SENT,
+    PAID,
+    FAILED
+
+}
