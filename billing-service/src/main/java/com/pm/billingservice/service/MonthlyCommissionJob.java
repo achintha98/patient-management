9d7c899519d1a50ef15afb06719404c538d71a52
achintha98 <kalanaachintha@gmail.com>
2025-10-22 23:55:19 +0530

    removed scheduling and supported the controller

--- billing-service/src/main/java/com/pm/billingservice/service/MonthlyCommissionJob.java
+++ billing-service/src/main/java/com/pm/billingservice/service/MonthlyCommissionJob.java
@@ -1,13 +1,21 @@
 package com.pm.billingservice.service;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.time.ZoneOffset;
 
 /**
@@ -16,6 +24,8 @@ import java.time.ZoneOffset;
  */
 
 @Service
+@RequiredArgsConstructor
+@Profile("master")
 public class MonthlyCommissionJob {
 
     private static final Logger logger = LoggerFactory.getLogger(MonthlyCommissionJob.class);
@@ -23,23 +33,34 @@ public class MonthlyCommissionJob {
     @Autowired
     private ScheduledJobService scheduledJobService;
 
-    @Scheduled(cron = "0 0 0 1 * *", zone = "UTC")
-    @Transactional
-    public void generateMonthlyinvoice() {
+    @Autowired
+    private final JobLauncher jobLauncher;
+
+    @Autowired
+    private final Job monthlyInvoiceJob;
+
+//    @Scheduled(cron = "0 0 0 1 * *", zone = "UTC")
+//@Scheduled(initialDelay = 0, fixedRate = 600000) // 600000 ms = 10 minutes
+    public void generateMonthlyInvoice() {
         String jobName = "MONTHLY_invoice";
         LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
 
         // check if already run this month
-        if (scheduledJobService.hasRunThisMonth(jobName, now)) {
-            logger.info("Skipping job {}, already executed for month {}", jobName, now.getMonth());
-            return;
-        }
+//        if (scheduledJobService.hasRunThisMonth(jobName, now)) {
+//            logger.info("Skipping job {}, already executed for month {}", jobName, now.getMonth());
+//            return;
+//        }
 
         try {
             logger.info("Running monthly invoice for {}", now.getMonth());
 
             // your main logic here
-            runInvoiceGeneration();
+            JobParameters params = new JobParametersBuilder()
+                    .addString("month", YearMonth.now().minusMonths(1).toString()) // previous month
+                    .addLong("time", System.currentTimeMillis()) // ensures uniqueness
+                    .toJobParameters();
+
+            jobLauncher.run(monthlyInvoiceJob, params);
 
             // mark job as completed
             scheduledJobService.updateLastRun(jobName, now);
